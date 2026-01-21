56def8f1c43069538a355353f3666d4d03f82747
Juan Roberto <Juan.santos@tivit.com>
2026-01-21 12:07:54 -0300

    fix: adiciona type module no package.json

--- index.js
+++ index.js
@@ -74,6 +74,9 @@ async function responderWhatssApp(numero, mensagem) {
     );
 }
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor rodando na porta 3000");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
